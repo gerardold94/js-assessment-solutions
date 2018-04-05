d1d5146c5f02554799eea4b8060a38000398024e
Gerardo Lopez Duenas <gerardo.lopez@magmalabs.io>
2018-04-05 16:11:25 -0500

    Add async solutions

--- app/async.js
+++ app/async.js
@@ -2,10 +2,17 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
   async: function(value) {
-
+    return new Promise(resolve => {
+      setTimeout(() => resolve(value), 250);
+    });
   },
 
   manipulateRemoteData: function(url) {
-
+    return new Promise(resolve => {
+      $.get(url, (data) => {
+        var people = data.people.map(person => person.name).sort();
+        resolve(people);
+      });
+    });
   }
 };
