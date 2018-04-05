6ba0a8cd73900d2cfbbc53592cdf99a8c21bc404
Gerardo Lopez Duenas <gerardo.lopez@magmalabs.io>
2018-04-05 16:41:40 -0500

    Add count solution

--- app/count.js
+++ app/count.js
@@ -2,6 +2,18 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
   count: function (start, end) {
-
+    let timeUp;
+    doIt = () => {
+      console.log(start++);
+      if(start <= end) {
+        timeUp = setTimeout(doIt, 100);
+      }
+    }
+    doIt();
+    return {
+      cancel: () => {
+        timeUp && clearTimeout(timeUp);
+      }
+    }
   }
 };
