d70daa30b4328c6d1511be1a0879d089a0b20997
Gerardo Lopez Duenas <gerardo.lopez@magmalabs.io>
2018-04-06 10:29:18 -0500

    Add objects solution

--- app/objects.js
+++ app/objects.js
@@ -2,14 +2,20 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.objectsAnswers = {
   alterContext: function(fn, obj) {
-
+    return fn.call(obj);
   },
 
   alterObjects: function(constructor, greeting) {
-
+    constructor.prototype.greeting = greeting;
   },
 
   iterate: function(obj) {
-
+    let properties = [];
+    for (let prop in obj) {
+      if (obj.hasOwnProperty(prop)) {
+        properties.push(`${prop}: ${obj[prop]}`);
+      }
+    }
+    return properties;
   }
 };
