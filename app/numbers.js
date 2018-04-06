a1a5e272832f6ad3f7db0ae3aae0234576f263c9
Gerardo Lopez Duenas <gerardo.lopez@magmalabs.io>
2018-04-06 10:19:13 -0500

    Add numbers solution

--- app/numbers.js
+++ app/numbers.js
@@ -2,18 +2,35 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.numbersAnswers = {
   valueAtBit: function(num, bit) {
-
+    return 1 & (num >> (bit-1));
   },
 
   base10: function(str) {
-
+    return parseInt(str, 2);
   },
 
   convertToBinary: function(num) {
-
+    let bin = [];
+    for (let i = 7; i >= 0; i--) {
+      bin.push(num & (1 << i) ? 1 : 0);
+    }
+    return bin.join('');
   },
 
   multiply: function(a, b) {
+    a = adjust(a);
+    b = adjust(b);
 
+    return (a.adjusted * b.adjusted) / (a.multiplier * b.multiplier);
+
+    function adjust(num) {
+      var exponent, multiplier;
+      if (num < 1) {
+        exponent = Math.floor( Math.log(num) * -1  ); // zeros after dot
+        multiplier = Math.pow(10, exponent);
+        return {adjusted: num * multiplier, multiplier: multiplier};
+      }
+      return {adjusted: num, multiplier: 1};
+    }
   }
 };
